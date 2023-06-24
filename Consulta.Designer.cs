63b00ba5535d5dcd8329983f80ab4918c57eb0d9
RuiMarrafa <rui.marrafa@my.istec.pt>
2023-06-24 19:23:25 +0100

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

---
+++ Consulta.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace veterinario_projeto
+{
+    partial class Consulta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_donoAnimal = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_dono = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.btn_cancelarAnimal = new System.Windows.Forms.Button();
+            this.btn_limparAnimal = new System.Windows.Forms.Button();
+            this.btn_salvarAnimal = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_donoAnimal
+            // 
+            this.lbl_donoAnimal.AutoSize = true;
+            this.lbl_donoAnimal.Font = new System.Drawing.Font("Century", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.lbl_donoAnimal.Location = new System.Drawing.Point(121, 56);
+            this.lbl_donoAnimal.Name = "lbl_donoAnimal";
+            this.lbl_donoAnimal.Size = new System.Drawing.Size(156, 23);
+            this.lbl_donoAnimal.TabIndex = 9;
+            this.lbl_donoAnimal.Text = "Id da Consulta";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(21, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(256, 23);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Id do Animal Consultado";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(265, 23);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Id do Médico Encarregado";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(94, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(178, 23);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Tipo de Consulta";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(94, 213);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(183, 23);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Data da Consulta";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(152, 255);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(125, 23);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Diagnóstico";
+            // 
+            // txt_dono
+            // 
+            this.txt_dono.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_dono.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txt_dono.Location = new System.Drawing.Point(294, 50);
+            this.txt_dono.Name = "txt_dono";
+            this.txt_dono.PlaceholderText = "Ex: 567";
+            this.txt_dono.Size = new System.Drawing.Size(476, 29);
+            this.txt_dono.TabIndex = 15;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(294, 89);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PlaceholderText = "Ex: 12";
+            this.textBox1.Size = new System.Drawing.Size(476, 29);
+            this.textBox1.TabIndex = 16;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox2.Location = new System.Drawing.Point(294, 127);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PlaceholderText = "Ex: 48";
+            this.textBox2.Size = new System.Drawing.Size(476, 29);
+            this.textBox2.TabIndex = 17;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox3.Location = new System.Drawing.Point(294, 167);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.PlaceholderText = "Ex: Vacinação";
+            this.textBox3.Size = new System.Drawing.Size(476, 29);
+            this.textBox3.TabIndex = 18;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox4.Location = new System.Drawing.Point(294, 207);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.PlaceholderText = "Ex: 11/9/2001";
+            this.textBox4.Size = new System.Drawing.Size(476, 29);
+            this.textBox4.TabIndex = 19;
+            // 
+            // textBox5
+            // 
+            this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox5.Location = new System.Drawing.Point(294, 249);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.PlaceholderText = "Ex: Animal Morreu";
+            this.textBox5.Size = new System.Drawing.Size(476, 29);
+            this.textBox5.TabIndex = 20;
+            // 
+            // btn_cancelarAnimal
+            // 
+            this.btn_cancelarAnimal.BackColor = System.Drawing.Color.LightGray;
+            this.btn_cancelarAnimal.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_cancelarAnimal.Location = new System.Drawing.Point(432, 350);
+            this.btn_cancelarAnimal.Name = "btn_cancelarAnimal";
+            this.btn_cancelarAnimal.Size = new System.Drawing.Size(140, 66);
+            this.btn_cancelarAnimal.TabIndex = 23;
+            this.btn_cancelarAnimal.Text = "Listar";
+            this.btn_cancelarAnimal.UseVisualStyleBackColor = false;
+            // 
+            // btn_limparAnimal
+            // 
+            this.btn_limparAnimal.BackColor = System.Drawing.Color.LightGray;
+            this.btn_limparAnimal.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_limparAnimal.Location = new System.Drawing.Point(228, 350);
+            this.btn_limparAnimal.Name = "btn_limparAnimal";
+            this.btn_limparAnimal.Size = new System.Drawing.Size(140, 66);
+            this.btn_limparAnimal.TabIndex = 22;
+            this.btn_limparAnimal.Text = "Limpar";
+            this.btn_limparAnimal.UseVisualStyleBackColor = false;
+            // 
+            // btn_salvarAnimal
+            // 
+            this.btn_salvarAnimal.BackColor = System.Drawing.Color.LightGray;
+            this.btn_salvarAnimal.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_salvarAnimal.Location = new System.Drawing.Point(21, 350);
+            this.btn_salvarAnimal.Name = "btn_salvarAnimal";
+            this.btn_salvarAnimal.Size = new System.Drawing.Size(140, 66);
+            this.btn_salvarAnimal.TabIndex = 21;
+            this.btn_salvarAnimal.Text = "Salvar";
+            this.btn_salvarAnimal.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightGray;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(630, 350);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 66);
+            this.button1.TabIndex = 24;
+            this.button1.Text = "Cancelar";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // Consulta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Azure;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_cancelarAnimal);
+            this.Controls.Add(this.btn_limparAnimal);
+            this.Controls.Add(this.btn_salvarAnimal);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txt_dono);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_donoAnimal);
+            this.Name = "Consulta";
+            this.Text = "Consulta";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label lbl_donoAnimal;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox txt_dono;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private Button btn_cancelarAnimal;
+        private Button btn_limparAnimal;
+        private Button btn_salvarAnimal;
+        private Button button1;
+    }
+}
